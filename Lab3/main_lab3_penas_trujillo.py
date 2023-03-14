7baa295ffcbf6ea52b3d323d4e8216bfb55eac7f
cpenas94 <henrypenas94@gmail.com>
2023-03-13 20:17:00 -0700

    Final Commit

--- Lab3/main_lab3_penas_trujillo.py
+++ Lab3/main_lab3_penas_trujillo.py
@@ -2,15 +2,26 @@ from module_lab3_Penas_Trujillo import *
 
 while True:
     user_input = input("Enter a word or sentence (type 'quit' to exit): ")
+    """
+    Choice of testing an input or closing the program
+    """
 
     if user_input.lower() == "quit":
         print("Goodbye!")
         break
+    """
+    Closes the program
+    """
+
+
+"""
+Prints whether input for each alphabetical antic is true or false
+"""
 
     print(f"'{user_input}' is:")
-    print(f"- a palindrome: {is_palindrome(user_input)}")
-    print(f"- a pangram: {is_pangram(user_input)}")
-    print(f"- a tautogram: {is_tautogram(user_input)}")
-    print(f"- an isogram: {is_isogram(user_input)}")
-    print(f"- an abecedarian: {is_abedecerian(user_input)}")
-    print(f"- a dobloon: {is_dobloon(user_input)}")
+    print(f"- Palindrome: {is_palindrome(user_input)}")
+    print(f"- Pangram: {is_pangram(user_input)}")
+    print(f"- Tautogram: {is_tautogram(user_input)}")
+    print(f"- Isogram: {is_isogram(user_input)}")
+    print(f"- Abecedarian: {is_abedecerian(user_input)}")
+    print(f"- Dobloon: {is_dobloon(user_input)}")
--- Lab3/module_lab3_Penas_Trujillo.py
+++ Lab3/module_lab3_Penas_Trujillo.py
@@ -1,27 +1,22 @@
-
 # CIS-117 Lab 3
 # Module to test alphabetical antics (or word play)
 # Group: 3
 # Authors: Henry Penas, Fernando Trujillo
 
-"""
-This module contains functions to test different types of word play:
-- Palindrome: a word or sentence that reads the same backwards
-- Pangram: a phrase or sentence containing all 26 letters of the alphabet
-- Tautogram: a text in which all words start with the same letter
-- Isogram: a word in which no letter of the alphabet occurs more than once
-- Abedecerian: a word in which the letters appear in alphabetical order
-- Dobloon: a word in which every letter that appears in the word appears exactly twice
-
-"""
-
-
 def is_palindrome(word):
+    """
+    Palindrome: a word or sentence that reads the same backwards
+    Returns true if input is a Palindrome, otherwise returns false if not.
+    """
     word = ''.join(filter(str.isalpha, word)).lower()
     return word == word[::-1]
 
 
 def is_pangram(sentence):
+    """
+    Pangram: a phrase or sentence containing all 26 letters of the alphabet
+    Returns true if input is a Pangram, otherwise returns false if not.
+    """
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     sentence = sentence.lower()
     for char in alphabet:
@@ -31,6 +26,10 @@ def is_pangram(sentence):
 
 
 def is_tautogram(sentence):
+    """
+    Tautogram: a text in which all words start with the same letter
+    Returns true if input is a Tautogram, otherwise returns false if not.
+    """
     words = sentence.split()
     first_char = words[0][0].lower()
     for word in words:
@@ -40,6 +39,10 @@ def is_tautogram(sentence):
 
 
 def is_isogram(word):
+    """
+    Isogram: a word in which no letter of the alphabet occurs more than once
+    Returns true if it input is a Isogram, otherwise returns false if not.
+    """
     for letter in word:
         if word.count(letter) > 1:
             return False
@@ -47,12 +50,19 @@ def is_isogram(word):
 
 
 def is_abedecerian(word):
+    """
+    Abedecerian: a word in which the letters appear in alphabetical order
+    Returns true if input is a Abedecerian, otherwise returns false if not.
+    """
     sorted_word = ''.join(sorted(word.lower()))
     return sorted_word == word.lower()
 
 
 def is_dobloon(word):
-
+    """
+    Dobloon: a word in which every letter that appears in the word appears exactly twice
+    Returns true if input is a Dobloon, otherwise returns false if not.
+    """
     for letter in word:
         if word.count(letter) != 2:
             return False
